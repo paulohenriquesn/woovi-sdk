023e7eff292e1b8cb77d7dd1465cc7f6db317b84
Paulo Henrique <contato@paulohenriquesn.com>
2023-07-22 10:20:14 -0300

    feat: ⭐ add charge service

---
+++ src/business/services/charge.ts
@@ -0,0 +1,17 @@
+import { STAGES } from '../../common/stages';
+import { Gateway } from '../../communication/protocols/gateway';
+import { httpResponse } from '../../communication/protocols/http';
+import { Charge } from '../contexts/charge/charge';
+import { CreateChargeCommand } from '../contexts/charge/commands/create';
+
+export class ChargeService implements Charge.Contracts {
+    constructor(
+        private readonly Authorization: string,
+        private readonly STAGE: STAGES,
+        private readonly createChargeGateway: Gateway<CreateChargeCommand.Input>
+    ) {}
+    async create(input: CreateChargeCommand.Input): Promise<httpResponse> {
+        return await this.createChargeGateway.execute(input, this.Authorization, this.STAGE)
+    }
+    
+}
