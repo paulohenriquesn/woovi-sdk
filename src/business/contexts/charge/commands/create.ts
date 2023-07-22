18fc683ffd5e26ddccf9def6e7e4bc55bab800dd
Paulo Henrique <contato@paulohenriquesn.com>
2023-07-22 09:35:28 -0300

    feat: 💸 add create charge contract

---
+++ src/business/contexts/charge/commands/create.ts
@@ -0,0 +1,23 @@
+import { httpResponse } from "../../../../communication/protocols/http"
+import { CHARGE_TYPE } from "../enums/type"
+
+export namespace CreateChargeCommand {
+    export type Input = {
+        correlationId: string
+        value: string
+        type?: CHARGE_TYPE
+        comment?: string
+        identifier?: string
+        expiresIn?: number
+        daysForDueDate?: number
+        daysAfterDueDate?: number
+        interests?: Record<string,any>,
+        additionalInfo?: Record<string,any>[]
+    }
+    
+    export type Output = httpResponse
+
+    export interface Contract {
+        create: (i: Input) => Promise<Output> 
+    }
+}
